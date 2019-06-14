fba6dbc078c33f6261711896b1c491501614323a
kachamaka <martin.popov42@gmail.com>
2019-06-14 21:44:26 +0300

    init commit

---
+++ argon2hash/argon2.go
@@ -0,0 +1,91 @@
+package argon2hash
+
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type Params struct {
+	memory      uint32
+	iterations  uint32
+	parallelism uint8
+	saltLength  uint32
+	keyLength   uint32
+}
+
+var P = Params{
+	memory:      64 * 1024,
+	iterations:  3,
+	parallelism: 2,
+	saltLength:  16,
+	keyLength:   32,
+}
+
+func GenerateFromPassword(password string) (encodedHash string, err error) {
+	salt, err := generateRandomBytes(P.saltLength)
+	if err != nil {
+		return "", err
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, P.iterations, P.memory, P.parallelism, P.keyLength)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	encodedHash = fmt.Sprintf("%s$%s", b64Salt, b64Hash)
+
+	return encodedHash, nil
+}
+
+func generateRandomBytes(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func ComparePasswordAndHash(password, encodedHash string) (match bool, err error) {
+	// Extract the parameters, salt and derived key from the encoded password
+	// hash.
+	salt, hash, err := decodeHash(encodedHash)
+	if err != nil {
+		return false, err
+	}
+
+	// Derive the key from the other password using the same parameters.
+	otherHash := argon2.IDKey([]byte(password), salt, P.iterations, P.memory, P.parallelism, P.keyLength)
+
+	// Check that the contents of the hashed passwords are identical. Note
+	// that we are using the subtle.ConstantTimeCompare() function for this
+	// to help prevent timing attacks.
+	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+		return true, nil
+	}
+	return false, nil
+}
+
+func decodeHash(encodedHash string) (salt, hash []byte, err error) {
+	vals := strings.Split(encodedHash, "$")
+
+	salt, err = base64.RawStdEncoding.DecodeString(vals[0])
+	if err != nil {
+		return nil, nil, err
+	}
+	P.saltLength = uint32(len(salt))
+
+	hash, err = base64.RawStdEncoding.DecodeString(vals[1])
+	if err != nil {
+		return nil, nil, err
+	}
+	P.keyLength = uint32(len(hash))
+
+	return salt, hash, nil
+}
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kachamaka/argon2/argon2hash"
+)
+
+func main() {
+
+	hash, err := argon2hash.GenerateFromPassword("testPass")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(hash)
+	match, err := argon2hash.ComparePasswordAndHash("testPass", hash)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(match)
+
+}
